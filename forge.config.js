d3fe1bb1290643b867bfe9ee50a15efb812d2af3
jsum01 <djadjawltjr@gmail.com>
2025-04-16 20:38:11 +0900

    chore: 미사용 플랫폼 빌드 스크립트 제거(linux)

--- forge.config.js
+++ forge.config.js
@@ -3,38 +3,50 @@ const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   packagerConfig: {
-    asar: true, 
+    asar: true,
     executableName: "CosmoLink",
-    icon: "./public/cosmoLink.ico",
-    // build 폴더를 앱 루트에 직접 복사
+    icon: "./public/icons/cosmoLink", // 확장자 제거
     extraFiles: [
       {
         from: "public/preload.js",
-        to: "preload.js"
+        to: "preload.js",
       },
       {
-        from: "build",
-        to: "build"
-      }
-    ]
+        from: "public/css",
+        to: "build/css",
+      },
+      {
+        from: "public/favicons",
+        to: "build/favicons",
+      },
+      {
+        from: "public/fonts",
+        to: "build/fonts",
+      },
+      {
+        from: "public/icons",
+        to: "build/icons",
+      },
+      {
+        from: "public/images",
+        to: "build/images",
+      },
+      {
+        from: "public/manifest.json",
+        to: "build/manifest.json",
+      },
+    ],
   },
   rebuildConfig: {},
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
       config: {},
+      platforms: ['win32'],
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      config: {},
+      platforms: ["darwin", "win32"],
     },
   ],
   plugins: [
@@ -48,7 +60,7 @@ module.exports = {
       [FuseV1Options.EnableCookieEncryption]: true,
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: false, // asar가 false일 때는 이것도 false로
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
       [FuseV1Options.OnlyLoadAppFromAsar]: false,
     }),
   ],
